c508a277710a7197b3f90bd295c36f8be82f22b2
PeterWhiteCL <101654874+PeterWhiteCL@users.noreply.github.com>
2023-04-19 11:31:08 -0300

    changes for demo hosting

--- public/app.js
+++ public/app.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
        
         try {
             const response = await fetch('/upload?filename='+ filename, {
-                method: 'POST',
+                method: 'GET',
                 headers: { 'Content-Type': 'application/json' },
                 // body: JSON.stringify({ filename}),
             
